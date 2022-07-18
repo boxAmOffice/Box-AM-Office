63d370378a3f1a4309b175b476e26c560c51bc7c
Sultan Kanaan <98963963+sultan-kanaan@users.noreply.github.com>
2022-07-18 11:54:52 +0300

    Add seed data

--- Box-AM-Ofice/Data/AppDbContext.cs
+++ Box-AM-Ofice/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Box_AM_Ofice.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,26 +8,11 @@ using System.Threading.Tasks;
 
 namespace boxAmOffice.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
-            {
-                am.ActorId,
-                am.MovieId
-            });
-
-            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
-            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
-
-
-            base.OnModelCreating(modelBuilder);
-        }
-
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
@@ -37,5 +24,78 @@ namespace boxAmOffice.Models
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+         
+            modelBuilder.Entity<Movie>().HasData(
+                        new Movie
+                        {   
+                            Id = 1,
+                            Name = "Life",
+                            Description = "This is the Life movie description",
+                            Price = 39.50,
+                            ImageURL = "",
+                            StartDate = DateTime.Now.AddDays(-10),
+                            EndDate = DateTime.Now.AddDays(10),
+                            CinemaId = 1,
+                            ProducerId = 1,
+                            MovieCategory = MovieCategory.Documentary
+                        }
+
+                        );
+
+            modelBuilder.Entity<Producer>().HasData(
+                new Producer
+                {
+                    Id = 1,
+                    FullName = "Producer 1",
+                    Bio = "This is the Bio of the first actor",
+                    ProfilePictureURL = ""
+
+                }
+
+                        );
+
+            modelBuilder.Entity<Actor>().HasData(
+                new Actor
+                {
+                    Id = 1,
+                    FullName = "Actor 1",
+                    Bio = "This is the Bio of the first actor",
+                    ProfilePictureURL = ""
+
+                }
+                );
+            modelBuilder.Entity<Cinema>().HasData(
+              new Cinema
+              {
+                  Id = 1,
+                  Name = "Cinema 1",
+                  Logo = "",
+                  Description = "This is the description of the first cinema"
+
+              }
+              );
+            modelBuilder.Entity<Actor_Movie>().HasData(
+                new Actor_Movie()
+                {
+                    ActorId = 1,
+                    MovieId = 1
+                }
+                );
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+
+        }
+
+       
     }
+
 }
--- Box-AM-Ofice/Models/Order.cs
+++ Box-AM-Ofice/Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Box_AM_Ofice.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace boxAmOffice.Models
 
         public string UserId { get; set; }
         [ForeignKey(nameof(UserId))]
-       // public ApplicationUser User { get; set; }
+        public ApplicationUser User { get; set; }
 
         public List<OrderItem> OrderItems { get; set; }
     }
