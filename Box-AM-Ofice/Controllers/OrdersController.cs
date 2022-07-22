08633b0477819c463d3fda4714245fbe9a498b97
HamZa Akram Samha <smha.hmza@gmail.com>
2022-07-22 21:30:38 +0300

    Design the NavBar

--- Box-AM-Ofice/Controllers/OrdersController.cs
+++ Box-AM-Ofice/Controllers/OrdersController.cs
@@ -2,83 +2,85 @@
 using Box_AM_Ofice.Models.Services;
 using Box_AM_Ofice.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Box_AM_Ofice.Controllers
 {
-    public class OrdersController : Controller
-    {
-        private readonly IMovie _movies;
-        private readonly ShoppingCart _shoppingCart;
-        private readonly IOrder _orders;
+	public class OrdersController : Controller
+	{
+		private readonly IMovie _movies;
+		private readonly ShoppingCart _shoppingCart;
+		private readonly IOrder _orders;
 
-        public OrdersController(IMovie movies, ShoppingCart shoppingCart, IOrder orders)
-        {
-            _movies = movies;
-            _shoppingCart = shoppingCart;
-            _orders = orders;
-        }
+		public OrdersController(IMovie movies, ShoppingCart shoppingCart, IOrder orders)
+		{
+			_movies = movies;
+			_shoppingCart = shoppingCart;
+			_orders = orders;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            string userRole = User.FindFirstValue(ClaimTypes.Role);
+		public async Task<IActionResult> Index()
+		{
+			string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			string userRole = User.FindFirstValue(ClaimTypes.Role);
 
-            var orders = await _orders.GetOrdersByUserIdAndRoleAsync(userId, userRole);
-            return View(orders);
-        }
+			var orders = await _orders.GetOrdersByUserIdAndRoleAsync(userId, userRole);
+			return View(orders);
+		}
 
-        public IActionResult ShoppingCart()
-        {
-            var items = _shoppingCart.GetShoppingCartItems();
-            _shoppingCart.ShoppingCartItems = items;
+		public IActionResult ShoppingCart()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
 
-            var response = new ShoppingCartVM()
-            {
-                ShoppingCart = _shoppingCart,
-                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
-            };
+			var response = new ShoppingCartVM()
+			{
+				ShoppingCart = _shoppingCart,
+				ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+			};
 
-            return View(response);
-        }
+			return View(response);
+		}
 
-        public async Task<IActionResult> AddItemToShoppingCart(int id)
-        {
-            var item = await _movies.GetMovie(id);
+		public async Task<IActionResult> AddItemToShoppingCart(int id)
+		{
+			var item = await _movies.GetMovie(id);
 
-            if (item != null)
-            {
-                _shoppingCart.AddItemToCart(item);
-            }
-            return RedirectToAction(nameof(ShoppingCart));
-        }
+			if (item != null)
+			{
+				_shoppingCart.AddItemToCart(item);
+			}
+			return RedirectToAction(nameof(ShoppingCart));
+		}
 
-        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
-        {
-            var item = await _movies.GetMovie(id);
+		public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+		{
+			var item = await _movies.GetMovie(id);
 
-            if (item != null)
-            {
-                _shoppingCart.RemoveItemFromCart(item);
-            }
-            return RedirectToAction(nameof(ShoppingCart));
-        }
+			if (item != null)
+			{
+				_shoppingCart.RemoveItemFromCart(item);
+			}
+			return RedirectToAction(nameof(ShoppingCart));
+		}
 
-        public async Task<IActionResult> CompleteOrder()
-        {
-            var items = _shoppingCart.GetShoppingCartItems();
-            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
+		public async Task<IActionResult> CompleteOrder()
+		{
+			if (!User.Identity.IsAuthenticated)
+			{
+				return View("LoginMessage");
+			}
 
-            await _orders.StoreOrderAsync(items, userId, userEmailAddress);
-            await _shoppingCart.ClearShoppingCartAsync();
+			var items = _shoppingCart.GetShoppingCartItems();
+			string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
 
-            return View("OrderCompleted");
-        }
-    }
+			await _orders.StoreOrderAsync(items, userId, userEmailAddress);
+			await _shoppingCart.ClearShoppingCartAsync();
+
+			return View("OrderCompleted");
+		}
+	}
 }
 
