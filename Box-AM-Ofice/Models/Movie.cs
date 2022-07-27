3fd9c971fb937fc22015921d65c2ae0674428c09
HamZa Akram Samha <smha.hmza@gmail.com>
2022-07-27 13:11:18 +0300

    Fix Styling

--- Box-AM-Ofice/Models/Movie.cs
+++ Box-AM-Ofice/Models/Movie.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace boxAmOffice.Models
 {
@@ -17,20 +15,32 @@ namespace boxAmOffice.Models
 
 
         [Required(ErrorMessage = "ImageURL is required")]
+        [Display(Name = "Image")]
+
         public string ImageURL { get; set; }
+
+        [Display(Name = "Strat Date")]
+
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+
         public DateTime EndDate { get; set; }
+
+        [Display(Name = "Category")]
         public MovieCategory MovieCategory { get; set; }
 
         //Relationships
         public List<Actor_Movie> Actors_Movies { get; set; }
 
         //Cinema
+        [Display(Name = "Cinema")]
         public int CinemaId { get; set; }
         [ForeignKey("CinemaId")]
         public Cinema Cinema { get; set; }
 
         //Producer
+        [Display(Name = "Producer")]
         public int ProducerId { get; set; }
         [ForeignKey("ProducerId")]
         public Producer Producer { get; set; }
